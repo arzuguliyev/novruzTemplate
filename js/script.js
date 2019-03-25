95b1ddc4c2f1952311f587e552b344041ca5f504
arzuguliyev <arzuquluxan@gmail.com>
2019-03-25 09:22:11 +0400

    Novruz Template

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+
+$("#iconCard1").hover(function(){
+    $("#mainTopIcon1 i").css("color", "white");
+    $("#mainTopIcon1").css("background-color", "#FC7F0C");
+    $("#mainTopIcon1").css("border", "none");
+     }, function(){
+     $("#mainTopIcon1").css("background-color", "white");
+     $("#mainTopIcon1 i").css("color", "gray");
+     $("#mainTopIcon1").css("border", "1px solid silver");
+   });
+
+
+
+$("#iconCard2").hover(function(){
+    $("#mainTopIcon2 i").css("color", "white");
+    $("#mainTopIcon2").css("background-color", "#FC7F0C");
+    $("#mainTopIcon2").css("border", "none");
+     }, function(){
+     $("#mainTopIcon2").css("background-color", "white");
+     $("#mainTopIcon2 i").css("color", "gray");
+     $("#mainTopIcon2").css("border", "1px solid silver");
+   });
+
+
+   $("#iconCard3").hover(function(){
+    $("#mainTopIcon3 i").css("color", "white");
+    $("#mainTopIcon3").css("background-color", "#FC7F0C");
+    $("#mainTopIcon3").css("border", "none");
+     }, function(){
+     $("#mainTopIcon3").css("background-color", "white");
+     $("#mainTopIcon3 i").css("color", "gray");
+     $("#mainTopIcon3").css("border", "1px solid silver");
+   });
+ 
+
+
+   $("#iconCard4").hover(function(){
+    $("#mainTopIcon4 i").css("color", "white");
+    $("#mainTopIcon4").css("background-color", "#FC7F0C");
+    $("#mainTopIcon4").css("border", "none");
+     }, function(){
+     $("#mainTopIcon4").css("background-color", "white");
+     $("#mainTopIcon4 i").css("color", "gray");
+     $("#mainTopIcon4").css("border", "1px solid silver");
+   });
+
+
+
+   
+ 
+ 
